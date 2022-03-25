f7062915abaa366ff06edddaa99e6f9c9aef6d44
HubTea <k3392255@naver.com>
2022-03-26 02:32:31 +0900

    Authorizer 클래스 작성

---
+++ src/authorizer.js
@@ -0,0 +1,67 @@
+/**
+ * 리소스에 대한 접근 제어를 하는 클래스.
+ * token 객체와 userId, mediaId를 비교하여 해당 리소스에 접근
+ * 가능한지 여부를 반환.
+ */
+class Authorizer{
+    /**
+     * 객체 초기화.
+     * @param {string | object} token 
+     * JSON or object
+     * ```
+     * {
+     *      user: {
+     *          userId
+     *      },
+     *      
+     *      media: {
+     *          mediaId,
+     *          region,
+     *          minimumAge
+     *      }
+     * }
+     * ```
+     */
+    constructor(token){
+        if(typeof(token) == 'string'){
+            this.token = JSON.stringify(token);
+        }
+        else{
+            this.token = token;
+        }
+    }
+
+    /**
+     * 
+     * @returns {string} Autorizer.token을 JSON으로 변환하여 반환.
+     */
+    Export(){
+        return JSON.stringify(this.token);
+    }
+
+    /**
+     * 
+     * @param {string} userId 유저의 uuid.
+     * @returns {boolean} userId에 해당하는 유저의 데이터에 접근 가능하면 true.
+     */
+    TestUserAccessibility(userId){
+        return (this.token.user.userId === userId);
+    }
+    
+    /**
+     * 
+     * @param {string} mediaId 
+     * @returns mediaId에 해당하는 미디어에 접근 가능하면 true.
+     */
+    TestMediaAccessibility(mediaId){
+        return true;
+    }
+
+    /**
+     * Authorizer.token의 userId를 설정함.
+     * @param {string} userId 유저의 uuid.
+     */
+    SetAccessibleUser(userId){
+        this.token.user.userId = userId;
+    }
+}
