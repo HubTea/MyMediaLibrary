86a996c633bd72118fc4b628466cb294ff09d59f
HubTea <k3392255@naver.com>
2022-03-29 22:39:05 +0900

    설정 추가
    개인키, 공개키, PBKDF2 파라미터 추가

--- src/serverConfig.js
+++ src/serverConfig.js
@@ -1,10 +1,30 @@
 //서버 포트, api버전, 데이터베이스 서버 주소 등의 정보를 담은 객체
 
 const process = require('process');
+const crypto = require('crypto');
+const fs = require('fs');
+
+const privateKey = crypto.createPrivateKey(
+    fs.readFileSync(process.env.PrivateKeyPath)
+);
+
+const publicKey = crypto.createPublicKey(privateKey);
 
 module.exports = {
     port: 443,
     apiVersion: 'v1',
+
+    key: {
+        private: privateKey,
+        public: publicKey
+    },
+    
+    pbkdf2: {
+        iteration: 30000,
+        hash: 'sha512',
+        digestLenght: 32
+    },
+
     sqlServer: {
         dbms: 'postgres',
         user: 'postgres',
