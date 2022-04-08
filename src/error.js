3a644d6bf75662499585aac5471a032a354241f3
HubTea <k3392255@naver.com>
2022-04-08 21:11:07 +0900

    변수명 및 주석 변경

--- src/error.js
+++ src/error.js
@@ -2,14 +2,16 @@
 class ErrorResponse{
     /**
      * 예외 발생 시 던져진 예외 객체외 더불어 추가적인 정보를 담아서 다시 던져지는 객체
-     * @param {number} httpErrorCode - 응답으로 전달될 HTTP 상태 코드
-     * @param {object} jsonErrorCode - 에러 코드를 나타내는 ErrorResponse의 정적 객체
-     * @param {string} message - 에러에 대한 구체적인 설명
-     * @param {object} underlyingError - 원인이 되는 err 객체
+     * @param {number} httpErrorCode 응답으로 전달될 HTTP 상태 코드
+     * @param {object} errorCode 
+     *      에러 종류를 나타내는 ENUM값이자 함수. 
+     *      ErrorResponse의 정적 멤버를 가리킴
+     * @param {string} message 에러에 대한 구체적인 설명
+     * @param {object} underlyingError 원인이 되는 err 객체
      */
-    constructor(httpErrorCode, jsonErrorCode, message, underlyingError){
+    constructor(httpErrorCode, errorCode, message, underlyingError){
         this.httpErrorCode = httpErrorCode;
-        this.jsonErrorCode = jsonErrorCode;
+        this.errorCode = errorCode;
         this.message = message;
         this.underlyingError = underlyingError;
     }
@@ -18,8 +20,8 @@ class ErrorResponse{
         return this.httpErrorCode;
     }
 
-    getJsonErrorCode(){
-        return this.jsonErrorCode;
+    getErrorCode(){
+        return this.errorCode;
     }
 
     getMessage(){
