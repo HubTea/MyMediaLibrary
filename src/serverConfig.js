6e0ca1ecce8032db233044f84a6c2ac3fb77b59d
HubTea <k3392255@naver.com>
2022-04-12 23:54:56 +0900

    테스트용 데이터베이스 설정을 분리

--- src/serverConfig.js
+++ src/serverConfig.js
@@ -1,4 +1,16 @@
-//서버 포트, api버전, 데이터베이스 서버 주소 등의 정보를 담은 객체
+/**
+ * 서버 포트, api버전, 데이터베이스 서버 주소 등의 정보를 담은 객체
+ * 
+ * 테스트 환경에서 정의되어야 하는 환경변수
+ *      PostgreSQLPassword: 테스트용 데이터베이스 서버 비밀번호
+ * 
+ * 프로덕션 환경에서만 정의되어야 하는 환경변수
+ *      ProductionMyMediaLibrary: 아무런 값이라도 정의돼있으면 됨.
+ *      ProductionPostgreSQLPassword: 데이터베이스 서버 비밀번호
+ * 
+ * 공통적으로 설정이 필요한 환경변수
+ *      PrivateKeyPath: 개인키를 저장하고 있는 pem 파일의 경로.
+ */
 
 const process = require('process');
 const crypto = require('crypto');
@@ -14,20 +26,39 @@ const privateKey = crypto.createPrivateKey(
 
 const publicKey = crypto.createPublicKey(privateKey);
 
-const sqlServer = {
-    dbms: 'postgres',
-    user: 'postgres',
-    password: process.env.PostgreSQLPassword,
-    host: 'localhost',
-    port: '5432',
-    database: 'MyMediaLibrary',
+let sqlServer;
 
-    get url(){
-        return `${this.dbms}://${this.user}:${this.password}@${this.host}:${this.port}/${this.database}`;
-    }
-};
+if(process.env.ProductionMyMediaLibrary){
+    sqlServer = {
+        dbms: 'postgres',
+        user: 'postgres',
+        password: process.env.ProductionPostgreSQLPassword,
+        host: '',
+        port: '',
+        database: 'MyMediaLibrary',
+    
+        get url(){
+            return `${this.dbms}://${this.user}:${this.password}@${this.host}:${this.port}/${this.database}`;
+        }
+    };
+}
+else{
+    sqlServer = {
+        dbms: 'postgres',
+        user: 'postgres',
+        password: process.env.PostgreSQLPassword,
+        host: 'localhost',
+        port: '5432',
+        database: 'MyMediaLibrary',
+    
+        get url(){
+            return `${this.dbms}://${this.user}:${this.password}@${this.host}:${this.port}/${this.database}`;
+        }
+    };
+}
 
 const sequelize = new orm.Sequelize(sqlServer.url);
+
 
 module.exports = {
     port: 443,
