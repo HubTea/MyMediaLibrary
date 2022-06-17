93ab1e1afefeae89167f1dd839ef52535590ba05
HubTea <k3392255@naver.com>
2022-06-17 18:23:44 +0900

    Request 클래스 추가
    기존의 sendRequest를 대체하기 위한 Request 클래스를 추가함.

--- test/testUtil.js
+++ test/testUtil.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 /**
  * 
- * @param {object} options http모듈에서 http.request 메서드의 options
+ * @param {http.RequestOptions} options http모듈에서 http.request 메서드의 options
  * @param {Buffer | string} body http body
  * @returns {Promise<object>} 
  * ```
@@ -14,31 +14,113 @@ const http = require('http');
  */
 async function sendRequest(options, body){
     let chunks = [];
+    return new Promise(asyncRequest);
 
     function asyncRequest(resolve, reject){
-        let req = http.request(options, function(res){
-            res.on('data', function(chunk){
-                chunks.push(chunk);
-            });
+        let req = http.request(options, bindResponseListener);
+        req.on('error', reject);
+        req.write(body);
+        req.end();
 
-            res.on('end', function(){
+        function bindResponseListener(res){
+            res.on('error', reject);
+            res.on('data', appendChunk);
+            res.on('end', emitResponse);
+
+            function appendChunk(chunk){
+                chunks.push(chunk);
+            }
+
+            function emitResponse(){
                 resolve({
                     res, 
                     body: Buffer.concat(chunks)
                 });
-            });
-        });
-
-        req.on('error', reject);
-    
-        req.write(body);
-        req.end();
+            }
+        }
     }
-    
-    return new Promise(asyncRequest);
 }
 
 
+class Request{
+    constructor(){
+        this.init();
+    }
+
+    init(){
+        this.chunkList = [];
+        this.option = null;
+        this.requestBody = null;
+        this.resolveSend = null;
+        this.rejectSend = null;
+        this.response = null;
+        this.promiseSend = null;
+    }
+
+    async getResponse(){
+        let resultSend = await this.promiseSend;
+        return resultSend.response;
+    }
+
+    async getBodyBuffer(){
+        let resultSend = await this.promiseSend;
+        return resultSend.body;
+    }
+
+    async getBodyObject(){
+        let resultSend = await this.promiseSend;
+        return JSON.parse(resultSend.body);
+    }
+    
+    /**
+     * 
+     * @param {http.RequestOptions} options 
+     * @param {string | Buffer} body 
+     * @returns {Promise<object>}
+     * ```
+     * {
+     *      response: http.IncommingMessage,
+     *      body: Buffer
+     * }
+     * ```
+     */
+    send(option, body){
+        this.init();
+        this.option = option;
+        this.requestBody = body;
+        return (this.promiseSend = new Promise(this.fulfillSend.bind(this)));
+    }
+
+    fulfillSend(resolve, reject){
+        this.resolveSend = resolve;
+        this.rejectSend = reject;
+        let request = http.request(this.option, this.bindResponseListener.bind(this));
+        
+        request.on('error', reject);
+        request.write(this.requestBody);
+        request.end();
+    }
+
+    bindResponseListener(response){
+        this.response = response;
+        response.on('error', this.rejectSend);
+        response.on('data', this.appendChunk.bind(this));
+        response.on('end', this.emitResponse.bind(this));
+    }
+
+    appendChunk(chunk){
+        this.chunkList.push(chunk);
+    }
+
+    emitResponse(){
+        this.resolveSend({
+            response: this.response,
+            body: Buffer.concat(this.chunkList)
+        });
+    }
+}
+
 module.exports = {
-    sendRequest
+    Request: Request,
+    sendRequest: sendRequest
 };
