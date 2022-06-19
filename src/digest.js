c4ef5cb1a54563064de87b4387000bd15f7a7bd2
HubTea <k3392255@naver.com>
2022-06-19 21:19:00 +0900

    digest.js 수정
    Codec 클래스 추가함.
    추가된 Codec 클래스를 사용하는 것으로 다른 클래스를 수정함.
    Pbkdf2DigestGenerator 클래스 가독성 향상.

--- src/digest.js
+++ src/digest.js
@@ -1,21 +1,61 @@
 /**
- * Digest: password로부터 만들어낸 Buffer
- * Salt: string
+ * Digest: password로부터 만들어낸 Codec 객체
+ * Salt: 무작위 값으로 생성한 Codec 객체
  * DigestPair: Digest와 Salt의 쌍
- * DigestBase64: Digest를 base64로 인코딩하고 '='을 제거한 문자열
  */
 
 const crypto = require('crypto');
 
+const serverConfig = require('./serverConfig');
+
+
+
+class Codec{
+    constructor(){
+        this.buffer = null;
+    }
+
+    isEqual(codec){
+        return this.buffer.compare(codec.getBuffer()) === 0;
+    }
+
+    setBuffer(digestBuffer){
+        this.buffer = digestBuffer;
+    }
+
+    getBuffer(){
+        return Buffer.from(this.buffer);
+    }
+
+    setBase64(digestBase64){
+        this.buffer = Buffer.from(digestBase64, 'base64');
+    }
+
+    getBase64(){
+        return this.buffer.toString('base64').replace('=', '');
+    }
+}
+
 
 class DigestPair{
-    async isEqual(password, digestGenerator){
+    /**
+     * 
+     * @param {DigestGenerator} digestGenerator 
+     */
+    constructor(digestGenerator){
+        this.digestGenerator = digestGenerator;
+    }
+
+    /**
+     * 
+     * @param {string} password 
+     * @returns {boolean}
+     */
+    async isEqual(password){
         const digestPair = await this.getDigestPair();
-        const generatedDigest = await digestGenerator.generateDigest(
-            password, digestPair.salt, digestPair.digest.length
-        );
+        const generatedDigest = await this.digestGenerator.generateDigest(password, digestPair.salt);
         
-        return digestPair.digest.compare(generatedDigest) === 0;
+        return digestPair.digest.isEqual(generatedDigest);
     }
 
     /**
@@ -31,8 +71,8 @@ class DigestPair{
 
     /**
      * 
-     * @param {Buffer} digest 
-     * @param {string} salt 
+     * @param {Codec} digest 
+     * @param {Codec} salt 
      */
     async setDigestPair(digest, salt){}
 }
@@ -41,12 +81,12 @@ class DigestPair{
 class DatabaseDigestPair extends DigestPair{
     /**
      * 
-     * @param {Model} model 패스워드 정보를 담고 있는 테이블의 모델
      * @param {string} accountId 패스워드 주인인 사용자의 accountId
+     * @param {DigestGenerator} digestGenerator
      */
-    constructor(model, accountId){
-        super();
-        this.model = model;
+    constructor(accountId, digestGenerator){
+        super(digestGenerator);
+        this.model = serverConfig.model.User;
         this.accountId = accountId;
     }
 
@@ -64,26 +104,38 @@ class DatabaseDigestPair extends DigestPair{
     }
 
     async getDigestPair(){
-        let userRow = await this.getRow();
+        let user = await this.getRow();
+
+        let digest = new Codec();
+        digest.setBase64(user.accountPasswordHash);
+
+        let salt = new Codec();
+        salt.setBase64(user.accountPasswordSalt);
 
         return {
-            digest: Buffer.from(userRow.accountPasswordHash, 'base64'),
-            salt: userRow.accountPasswordSalt
+            digest: digest,
+            salt: salt
         };
     }
 
     async setDigestPair(digest, salt){
         let user = await this.getRow();
-        user.accountPasswordHash = digest.toString('base64');
-        user.accountPasswordSalt = salt;
+        user.accountPasswordHash = digest.getBase64();
+        user.accountPasswordSalt = salt.getBase64();
         await user.save();
     }
 }
 
 
 class ConstantDigestPair extends DigestPair{
-    constructor(digest, salt){
-        super();
+    /**
+     * 
+     * @param {Codec} digest 
+     * @param {Codec} salt 
+     * @param {DigestGenerator} digestGenerator 
+     */
+    constructor(digest, salt, digestGenerator){
+        super(digestGenerator);
         this.setDigestPair(digest, salt);
     }
 
@@ -105,39 +157,52 @@ class DigestGenerator{
     /**
      * 
      * @param {string} password 
-     * @param {string} salt 
-     * @param {number} digestLength 
-     * @returns {Promise<Buffer>}
+     * @param {Codec} salt 
+     * @returns {Promise<Codec>}
      */
-    async generateDigest(password, salt, digestLength){}
+    generateDigest(password, salt){}
 }
 
 
 class Pbkdf2DigestGenerator extends DigestGenerator{
-    constructor(iteration, hash){
+    constructor(iteration, hash, digestLength){
         super();
         this.iteration = iteration;
         this.hash = hash;
+        this.digestLength = digestLength;
+
+        this.password = null;
+        this.salt = null;
+
+        this.resolveGenerateDigest = null;
+        this.rejectGenerateDigest = null;
     }
     
-    async generateDigest(password, salt, digestLength){
-        function asyncPbkdf2(resolve, reject){
-            crypto.pbkdf2(password, salt, this.iteration, digestLength, this.hash, 
-                function(err, digest){
-                    if(err){
-                        reject(err);
-                    }
-                    else{
-                        resolve(digest);
-                    }
-                }
-            );
-        }
+    generateDigest(password, salt){
+        this.password = password;
+        this.salt = salt;
+        return new Promise(this.fulfillGenerateDigest.bind(this));
+    }
 
-        return new Promise(asyncPbkdf2.bind(this));
+    fulfillGenerateDigest(resolve, reject){
+        this.resolveGenerateDigest = resolve;
+        this.rejectGenerateDigest = reject;
+        crypto.pbkdf2(this.password, this.salt.getBuffer(), this.iteration, this.digestLength, this.hash, this.emitDigest.bind(this));
+    }
+
+    emitDigest(err, digestBuffer){
+        if(err){
+            this.rejectGenerateDigest(err);
+        }
+        else{
+            let digest = new Codec();
+            digest.setBuffer(digestBuffer);
+            this.resolveGenerateDigest(digest);
+        }
     }
 }
 
+exports.Codec = Codec;
 exports.DatabaseDigestPair = DatabaseDigestPair;
 exports.ConstantDigestPair = ConstantDigestPair;
 exports.Pbkdf2DigestGenerator = Pbkdf2DigestGenerator;
