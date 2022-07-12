24c6db04184115971dffd18d45e60df84f572f0f
HubTea <k3392255@naver.com>
2022-07-13 00:59:49 +0900

    repository 모듈 작성함
    repository는 sequelize와 데이터베이스 스키마를 래핑한 모듈.
    아직은 Users 테이블에 대한 코드만 작성됨.
    
    이 모듈의 함수 내에서 sequelize에 접근함.
    이 모듈이 다른 모듈과 주고받는 객체는
    데이터베이스 테이블 스키마와는 형태가 약간 다름.
    이 모듈이 다른 모듈과 주고받는 객체와 sequelize 모델를
    양방향으로 변환해주는 함수도 있음.
    
    모듈을 통해 데이터베이스 접근에 대한 관심을 분리함.

---
+++ src/repository/userRepository.js
@@ -0,0 +1,178 @@
+const sequelize = require('sequelize');
+
+const serverConfig = require('../serverConfig');
+const error = require('../error');
+
+
+/**
+ * 
+ * @param {string} userId 
+ * @returns {Promise<object>} user value object
+ */
+async function getUserByUserId(userId){
+    return await getOneUser({
+        where: {
+            userId: userId
+        }
+    });
+}
+
+/**
+ * 
+ * @param {string} accountId 
+ * @returns {Promise<object>} user value object
+ */
+async function getUserByAccountId(accountId){
+    return await getOneUser({
+        where: {
+            accountId: accountId
+        }
+    });
+}
+
+/**
+ * 
+ * @param {object} queryOption sequelize의 findOne 메소드에 전달할 인자
+ * @returns {Promise<object>} user value object
+ */
+async function getOneUser(queryOption){
+    try{
+        let user = await serverConfig.model.User.findOne(queryOption);
+
+        if(user === null){
+            throw new error.UserNotExistError(null);
+        }
+    
+        return userToValueObject(user);
+    }
+    catch(userFindError){
+        throw wrapSequelizeError(userFindError);
+    }
+}
+
+/**
+ * 
+ * @param {object} userSeed 
+ *  ```
+ * {
+ *  accountId: string,
+ *  nickname: string,
+ *  hash: string,
+ *  salt: string
+ * }
+ * ```
+ * @returns {Promise<object>} user value object
+ *
+ */
+async function createUser(userSeed){
+    try{
+        let user = userSeedToUser(userSeed);
+
+        user = await user.save();
+
+        return userToValueObject(user);
+    }
+    catch(userCreateError){
+        throw wrapUserCreateError(userCreateError);
+    }
+}
+
+function userSeedToUser(userSeed){
+    return serverConfig.model.User.build({
+        accountId: userSeed.accountId,
+        nickname: userSeed.nickname,
+        accountPasswordHash: userSeed.hash,
+        accountPasswordSalt: userSeed.salt
+    });
+}
+
+function wrapUserCreateError(userCreationError){
+    if(userCreationError instanceof sequelize.UniqueConstraintError){
+        return new error.UserAlreadyExistError(userCreationError);
+    }
+    else{
+        return wrapSequelizeError(userCreationError);
+    }
+}
+
+async function setUser(userValueObject){
+    try{
+        let user = userValueObjectToUser(userValueObject);
+
+        let updateResult = await serverConfig.model.User.update({
+            accountId: user.accountId,
+            nickname: user.nickname,
+            introduction: user.introduction,
+            thumbnailUrl: user.thumbnailUrl,
+            accountPasswordHash: user.accountPasswordHash,
+            accountPasswordSalt: user.accountPasswordSalt
+        }, {
+            where: {
+                userId: userValueObject.userId
+            }
+        });
+
+        return userValueObject;
+    }
+    catch(userUpdateError){
+        throw wrapSequelizeError(userUpdateError);
+    }
+}
+
+function wrapSequelizeError(err){
+    if(err instanceof sequelize.BaseError){
+        return new error.DatabaseError(err);
+    }
+    else{
+        return err;
+    }
+}
+
+function userValueObjectToUser(userValueObject){
+    return serverConfig.model.User.build({
+        userId: userValueObject.userId,
+        accountId: userValueObject.accountId,
+        nickname: userValueObject.nickname,
+        introduction: userValueObject.introduction,
+        thumbnailUrl: userValueObject.thumbnailUrl,
+        accountPasswordHash: userValueObject.hash,
+        accountPasswordSalt: userValueObject.salt
+    });
+}
+
+/**
+ * 
+ * @param {User} user 
+ * @returns {object} user value object
+ * ```
+ * {
+ *  userId: string,
+ *  accountId: string,
+ *  nickname: string,
+ *  introduction: string,
+ *  thumbnailUrl: string,
+ *  hash: string,
+ *  salt: string
+ * }
+ * ```
+ */
+ function userToValueObject(user){
+    return {
+        userId: user.userId,
+        accountId: user.accountId,
+        nickname: user.nickname,
+        introduction: user.introduction,
+        thumbnailUrl: user.thumbnailUrl,
+        hash: user.accountPasswordHash,
+        salt: user.accountPasswordSalt
+    };
+}
+
+module.exports = {
+    getUserByUserId,
+    getUserByAccountId,
+
+    createUser,
+
+    setUser
+};
