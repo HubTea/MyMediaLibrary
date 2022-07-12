eb6984dfd65264b3957d567acaa885ca8d5fc82a
HubTea <k3392255@naver.com>
2022-07-13 01:02:09 +0900

    테스트케이스에 대한 설명 변경

--- test/testPatchUserMetadata.js
+++ test/testPatchUserMetadata.js
@@ -34,11 +34,14 @@ async function testPatchUserMetadata({accountId, nickname, introduction}){
 }
 
 describe('PATCH /v1/users/{userId}/info 테스트', function(){
-    it('유효한 값으로 유저 정보 변경 테스트', async function(){
-        await testPatchUserMetadata({
-            accountId: 'tempAccountId',
-            nickname: 'tempNickname',
-            introduction: 'hello'
-        });
-    });
+    it(
+        '유효한 값으로 유저 정보 변경 요청 시 정상 응답 오는지 테스트(요청한 값이 DB에 반영되는지는 테스트 안 함)', 
+        async function(){
+            await testPatchUserMetadata({
+                accountId: 'tempAccountId',
+                nickname: 'tempNickname',
+                introduction: 'hello'
+            });
+        }
+    );
 });
