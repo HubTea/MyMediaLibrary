61b426a1abe67b90bcaf82f7bcf43a497a29d9fe
HubTea <k3392255@naver.com>
2022-07-15 00:46:54 +0900

    aws s3 클라이언트 생성 코드 수정

--- src/storageService.js
+++ src/storageService.js
@@ -1,10 +1,11 @@
 const {S3Client} = require('@aws-sdk/client-s3');
-
+const {fromEnv} = require('@aws-sdk/credential-providers');
 
 const bucket = process.env.AWS_BUCKET;
 const region = process.env.AWS_REGION;
 const client = new S3Client({
-    region: region
+    region: region,
+    credentials: fromEnv()
 });
 
 module.exports = {
