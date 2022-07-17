dafa68c0ee498d714aa4dbcd2379ac6e59de1e3d
HubTea <k3392255@naver.com>
2022-07-17 18:03:09 +0900

    GET /v1/medias/{mediaId}/info 코드 추가

--- src/mediaRouter.js
+++ src/mediaRouter.js
@@ -2,6 +2,7 @@ const express = require('express');
 const stream = require('stream/promises');
 
 const mediaRepository = require('./repository/mediaRepository');
+const userRepository = require('./repository/userRepository');
 const checker = require('./checker');
 const errorHandler = require('./errorhandler');
 
@@ -13,7 +14,38 @@ mediaRouter.get('/', function(req, res){
 });
 
 mediaRouter.get('/:mediaId/info', async function(req, res){
+    try{
+        let mediaId = req.params.mediaId;
+        let mediaEntity = mediaRepository.MediaEntity.fromId(mediaId);
+        
+        await mediaEntity.addViewCount(1);
 
+        let mediaValueObject = await mediaEntity.getMetadata();
+        let viewCount = await mediaEntity.getViewCount();
+        let dislikeCount = await mediaEntity.getDislikeCount();
+
+        let userId = mediaValueObject.uploader;
+        let userValueObject = await userRepository.getUserByUserId(userId);
+        
+        res.status(200);
+        res.set('Content-Type', 'application/json');
+        res.write(JSON.stringify({
+            title: mediaValueObject.title,
+            description: mediaValueObject.description,
+            type: mediaValueObject.type,
+            updateTime: mediaValueObject.updateTime,
+            viewCount: viewCount,
+            dislikeCount: dislikeCount,
+            uploader: {
+                userId: userId,
+                nickname: userValueObject.nickname
+            }
+        }));
+        res.end();
+    }
+    catch(err){
+        errorHandler.handleError(res, err);
+    }
 });
 
 mediaRouter.get('/:mediaId', async function(req, res){
