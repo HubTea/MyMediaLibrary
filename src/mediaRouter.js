46ff181ebcf696ac32e508afeca8453d1c9d4184
HubTea <k3392255@naver.com>
2022-07-24 16:07:58 +0900

    GET /v1/medias/{mediaUuid}/comments 코드 추가

--- src/mediaRouter.js
+++ src/mediaRouter.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const stream = require('stream/promises');
+const sequelize = require('sequelize');
 
 const mediaRepository = require('./repository/mediaRepository');
 const userRepository = require('./repository/userRepository');
 const checker = require('./checker');
 const errorHandler = require('./errorhandler');
+const serverConfig = require('./serverConfig');
 
 
 const mediaRouter = express.Router();
@@ -79,8 +81,175 @@ mediaRouter.post('/:mediaUuid', async function(req, res){
     }
 });
 
-mediaRouter.get('/:mediaUuid/comments', function(req, res){
+mediaRouter.get('/:mediaUuid/comments', async function(req, res){
+    let mediaUuid = req.params.mediaUuid;
+    let length = req.query.length;
+    let cursor = req.query.cursor;
+    let parentUuid = req.query.parentUuid;
+    let date;
+    let random;
+    let limit;
 
+    if(cursor){
+        let stringCursor = cursor.split('_');
+
+        date = new Date().setTime(parseInt(stringCursor[0]));
+        random = parseInt(stringCursor[1]);
+    }
+    else{
+        date = new Date('2000-01-01T00:00Z');
+        random = -1;
+    }
+
+    if(length){
+        length = parseInt(length);
+    }
+    else{
+        length = 50;
+    }
+    limit = length + 1;
+
+    if(parentUuid){
+        let parentComment = await serverConfig.model.Comment.findOne({
+            attributes: ['id'],
+            where: {
+                uuid: parentUuid
+            }
+        });
+        let childCommentList = await serverConfig.model.Comment.findAll({
+            attributes: ['uuid', 'content', 'createdAt', 'updatedAt', 'random'],
+            where: {
+                parentId: parentComment.id,
+                [sequelize.Op.or]: [{
+                    createdAt: {
+                        [sequelize.Op.gt]: date
+                    }
+                }, {
+                    createdAt: date,
+                    random: {
+                        [sequelize.Op.gte]: random
+                    }
+                }]
+            },
+            include: [{
+                model: serverConfig.model.User,
+                as: 'CommentWriter',
+                attributes: ['uuid', 'nickname']
+            }],
+            order: [
+                ['createdAt', 'ASC'],
+                ['random', 'ASC']
+            ],
+            limit: limit
+        });
+        
+        let resBody = {
+            list: []
+        };
+
+        if(childCommentList.length === 0){
+            res.json(resBody);
+            res.end();
+            return;
+        }
+
+        if(childCommentList[limit - 1]){
+            let nextComment = childCommentList[limit - 1];
+            let utcMs = nextComment.createdAt.getTime();
+            let random = nextComment.random;
+
+            resBody.nextCursor = `${utcMs}_${random}`;
+        }
+
+        let bound = Math.min(childCommentList.length, limit - 1);
+
+        for(let i = 0; i < bound; i++){
+            let comment = childCommentList[i];
+
+            resBody.list.push({
+                uuid: comment.uuid,
+                writer: {
+                    uuid: comment.CommentWriter.uuid,
+                    nickname: comment.CommentWriter.nickname
+                },
+                content: comment.content,
+                createdAt: comment.createdAt.toISOString(),
+                updatedAt: comment.updatedAt.toISOString()
+            });
+        }
+        res.set('Content-Type', 'application/json');
+        res.write(JSON.stringify(resBody, null, 5));
+        res.end();
+    }
+    else{
+        let mediaEntity = mediaRepository.MediaEntity.fromUuid(mediaUuid);
+        let mediaValueObject = await mediaEntity.getMetadata();
+
+        let parentCommentList = await serverConfig.model.Comment.findAll({
+            attributes: ['uuid', 'content', 'createdAt', 'updatedAt', 'random'],
+            where: {
+                mediaId: mediaValueObject.id,
+                parentId: {
+                    [sequelize.Op.is]: null
+                },
+                [sequelize.Op.or]: [{
+                    createdAt: {
+                        [sequelize.Op.gt]: date
+                    }
+                }, {
+                    [sequelize.Op.and]: [{
+                        createdAt: date
+                    }, {
+                        random: {
+                            [sequelize.Op.gte]: random
+                        }
+                    }]
+                }]
+            },
+            include: [{
+                model: serverConfig.model.User,
+                as: 'CommentWriter',
+                attributes: ['uuid', 'nickname']
+            }],
+            order: [
+                ['createdAt', 'ASC'],
+                ['random', 'ASC']
+            ],
+            limit: limit
+        });
+        
+        let resBody = {
+            list: []
+        };
+
+        if(parentCommentList[limit - 1]){
+            let nextComment = parentCommentList[limit - 1];
+            let utcMs = nextComment.createdAt.getTime();
+            let random = nextComment.random;
+
+            resBody.nextCursor = `${utcMs}_${random}`;
+        }
+
+        let bound = Math.min(parentCommentList.length, limit - 1);
+
+        for(let i = 0; i < bound; i++){
+            let comment = parentCommentList[i];
+
+            resBody.list.push({
+                commentUuid: comment.uuid,
+                writer: {
+                    userUuid: comment.CommentWriter.uuid,
+                    nickname: comment.CommentWriter.nickname
+                },
+                content: comment.content,
+                createdAt: comment.createdAt.toISOString(),
+                updatedAt: comment.updatedAt.toISOString()
+            });
+        }
+        res.set('Content-Type', 'application/json');
+        res.write(JSON.stringify(resBody, null, 5));
+        res.end();
+    }
 });
 
 module.exports = {
