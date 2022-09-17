bf0dc6d5a6f4d60d1ff26c40f6ffc6d10a00b279
HubTea <k3392255@naver.com>
2022-09-17 15:06:06 +0900

    로컬 개발 환경에서도 컨텐츠 파일 저장 시 AWS S3를 사용하도록 수정

--- src/storageService.js
+++ src/storageService.js
@@ -1,26 +1,15 @@
 const {S3Client} = require('@aws-sdk/client-s3');
 
 
-let bucket;
-let region;
-let client;
-
-try{
-    bucket = process.env.AWS_S3_BUCKET;
-    region = process.env.AWS_S3_REGION;
-    client = new S3Client({
-        region: region,
-        credentials: {
-            accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-            secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-        }
-    });
-}
-catch(err){
-    if(process.env.NODE_ENV === 'production'){
-        throw err;
+let bucket = process.env.AWS_S3_BUCKET;
+let region = process.env.AWS_S3_REGION;
+let client = new S3Client({
+    region: region,
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
     }
-}
+});
 
 module.exports = {
     client,
